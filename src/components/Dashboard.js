3867693e7a2efa3a2805d577562e41c288c4098f
Paul Vitalis <vitalispaul48@live.com>
2020-07-15 15:30:07 +0300

    Revert "Ft add product"

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,60 +1,26 @@
-import React, { useEffect } from 'react';
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { logoutUser } from '../redux/actions/authActions'
-import { Link } from 'react-router-dom';
+import React from 'react';
 
-const Dashboard = (props) => {
-  useEffect(() => {
-    if (props.auth.isAuthenticated === false) {
-      props.history.push('/')
-    }
-  }, [props.history, props.auth])
-  const handleLogout = (e) => {
-    e.preventDefault()
-    props.logoutUser()
-  }
-
-  return (
-    <div>
-      <div onClick={handleLogout}>
-        Logout
-      </div>
-      <div>
+const Dashboard = () => {
+    return (
         <div>
-          <p>Username: {localStorage.getItem('username')}</p>
-          <p>Role: {localStorage.getItem('role')}</p>
+            <nav></nav>
+            <div>
+                <div>
+                    <p>Username</p>
+                    <p>Role</p>
+                </div>
+                <div>
+                    <p>Create product</p>
+                    <p>Modify/Delete product</p>
+                </div>
+                <div>
+                    <p>Sign Up Employee</p>
+                    <p>View sales records</p>
+                </div>
+                <div></div>
+            </div>
         </div>
-        <div>
-          <Link to='/products'>
-            Products
-          </Link>
-        </div>
-        <div>
-          <p>
-            <Link to='/create-product'>
-              Create product
-            </Link>
-          </p>
-          <p>Modify/Delete product</p>
-        </div>
-        <div>
-          <p>Sign Up Employee</p>
-          <p>View sales records</p>
-        </div>
-        <div></div>
-      </div>
-    </div>
-  )
+    )
 }
 
-Dashboard.propTypes = {
-  logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
-}
-
-const mapStateToProps = state => ({
-  auth: state.auth
-})
-
-export default connect(mapStateToProps, { logoutUser })(Dashboard)
+export default Dashboard
--- src/redux/actions/products/productTypes.js
+++
@@ -1,4 +0,0 @@
-export const GET_ERRORS = 'GET_ERRORS'
-export const LOADING_PRODUCTS = 'LOADING_PRODUCTS'
-export const SET_PRODUCTS = 'SET_PRODUCTS'
-export const CREATE_PRODUCT = 'CREATE_PRODUCT'
